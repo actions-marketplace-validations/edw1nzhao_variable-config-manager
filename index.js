2c02420d4b4f576f2266edb4124b15a4fcf0f797
ezhao7 <edwin.zhao@outlook.com>
2021-02-22 13:42:25 -0800

    Commit

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const fs = require('fs');
 const yaml = require('js-yaml');
 const shell = require('shelljs');
 
+const SecretManagerServiceClient = require('@google-cloud/secret-manager');
+
+const client = new SecretManagerServiceClient();
+
 try {
     const configRepo = core.getInput('config-repo');
     const pat = core.getInput('pat')
@@ -25,3 +29,42 @@ try {
 } catch (error) {
     core.setFailed(error.message);
 }
+
+// parent = 'projects/my-project', // Project for which to manage secrets.
+// secretId = 'foo', // Secret ID.
+// payload = 'hello world!' // String source data.
+
+// async function createAndAccessSecret() {
+//     // Create the secret with automation replication.
+//     const [secret] = await client.createSecret({
+//         parent: parent,
+//         secret: {
+//         name: secretId,
+//         replication: {
+//             automatic: {},
+//         },
+//         },
+//         secretId,
+//     });
+
+//     console.info(`Created secret ${secret.name}`);
+
+//     // Add a version with a payload onto the secret.
+//     const [version] = await client.addSecretVersion({
+//         parent: secret.name,
+//         payload: {
+//         data: Buffer.from(payload, 'utf8'),
+//         },
+//     });
+
+//     console.info(`Added secret version ${version.name}`);
+
+//     // Access the secret.
+//     const [accessResponse] = await client.accessSecretVersion({
+//         name: version.name,
+//     });
+
+//     const responsePayload = accessResponse.payload.data.toString('utf8');
+//     console.info(`Payload: ${responsePayload}`);
+// }
+//   createAndAccessSecret();
